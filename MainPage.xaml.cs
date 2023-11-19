6991824865eb13d732fd1ebbcd23ec8a4f24ed57
hgalvarado <131572735+hgalvarado@users.noreply.github.com>
2023-11-19 17:59:40 -0600

    En estos cambios se agrego la ubicacion, la grabacion del video.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using CommunityToolkit.Maui.Views;
 using Microsoft.Maui.Storage;
-
 using Microsoft.Maui.Maps;
 using Plugin.Maui.Audio;
+using System.IO;
 
 
 namespace PM2Examen2Grupo1
@@ -15,82 +15,100 @@ namespace PM2Examen2Grupo1
         private readonly IAudioRecorder _audioRecorder;
         private bool isRecording = false;
         public string pathaudio, filename;
+
+        //obtener ubicacion Actual
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+
         public MainPage()
         {
             InitializeComponent();
             _audioRecorder = AudioManager.Current.CreateRecorder();
+            
         }
 
-        //protected async override void OnAppearing()
-        //{
-        //    base.OnAppearing();
+        public async Task GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
 
-        //    var connection = Connectivity.NetworkAccess;
-        //    //var local = CrossGeolocator.Current;
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
 
-        //    if (connection == NetworkAccess.Internet)
-        //    {
-        //        if (local == null || !local.IsGeolocationAvailable || !local.IsGeolocationEnabled)
-        //        {
-        //            // Si la geolocalización no está disponible o no está habilitada
-        //            await CheckAndRequestLocationPermissionAsync();
-        //        }
-        //        else
-        //        {
-        //            await GetLocationAsync();
-        //        }
-        //    }
-        //    else
-        //    {
-        //        await DisplayAlert("Sin Acceso a internet", "Por favor, revisa tu conexion a internet para continuar.", "OK");
-        //    }
-        //}
+                _cancelTokenSource = new CancellationTokenSource();
 
-        //private async Task CheckAndRequestLocationPermissionAsync()
-        //{
-        //    var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
 
-        //    if (status != PermissionStatus.Granted)
-        //    {
-        //        status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
-        //    }
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    txtLatitud.Text = location.Latitude.ToString();
+                    txtLongitud.Text = location.Longitude.ToString();
+                    await DisplayAlert("Mensaje", "Ubicacion lista", "OK");
+                }
+                    
 
-        //    if (status == PermissionStatus.Granted)
-        //    {
-        //        await GetLocationAsync();
-        //    }
-        //    else if (status == PermissionStatus.Denied)
-        //    {
-        //        await DisplayAlert("Advertencia", "Esta aplicacion no puede funcionar si no tiene los permisos", "OK");
-        //    }
-        //}
-        //public async Task GetLocationAsync()
-        //{
-        //    try
-        //    {
-        //        var request = new GeolocationRequest(GeolocationAccuracy.Medium);
-        //        var location = await Geolocation.GetLocationAsync(request);
+            }
+            // Catch one of the following exceptions:
+            //   FeatureNotSupportedException
+            //   FeatureNotEnabledException
+            //   PermissionException
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+        }
 
-        //        if (location != null)
-        //        {
-        //            txtLatitud.Text = "" + location.Latitude;
-        //            txtLongitud.Text = "" + location.Longitude;
-        //        }
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
 
-        //    }
-        //    catch (FeatureNotSupportedException fnsEx)
-        //    {
-        //        await DisplayAlert("Advertencia", fnsEx + "", "OK");
-        //    }
-        //    catch (PermissionException Ex)
-        //    {
-        //        await DisplayAlert("Advertencia", "Esta aplicacion no puede funcionar si no tiene los permisos", "OK");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        await DisplayAlert("Advertencia", ex + "", "OK");
-        //    }
-        //}
+
+
+        public async Task TakePhotoAsync()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult video = await MediaPicker.Default.CaptureVideoAsync();
+
+                if (video != null)
+                {
+                    string localFolderPath = Path.Combine(FileSystem.CacheDirectory, "Videos");
+                    Directory.CreateDirectory(localFolderPath); // Asegurarse de que la carpeta existe
+
+                    string localFilePath = Path.Combine(localFolderPath, video.FileName);
+
+                    using (Stream sourceStream = await video.OpenReadAsync())
+                    using (FileStream localFileStream = File.Create(localFilePath)) // Use 'File.Create' to ensure a new copy
+                    //using (FileStream localFileStream = File.OpenWrite(localFilePath))
+                    {
+                        await sourceStream.CopyToAsync(localFileStream);
+                    }
+
+
+                    // Suponiendo que `viewVideo` es un control MediaElement
+                    viewVideo.Source = MediaSource.FromFile(localFilePath);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -110,61 +128,9 @@ namespace PM2Examen2Grupo1
 
         }
 
-        //public async Task<bool> CheckAndRequestStoragePermission()
-        //{
-        //    var status = await Permissions.CheckStatusAsync<Permissions.StorageWrite>();
-        //    if (status != PermissionStatus.Granted)
-        //    {
-        //        status = await Permissions.RequestAsync<Permissions.StorageWrite>();
-        //    }
-
-        //    return status == PermissionStatus.Granted;
-        //}
-
-        public async Task<bool> CheckAndRequestPermissionAsync<T>() where T : Permissions.BasePermission, new()
-        {
-            var status = await Permissions.CheckStatusAsync<T>();
-            if (status != PermissionStatus.Granted)
-            {
-                status = await Permissions.RequestAsync<T>();
-                if (status != PermissionStatus.Granted)
-                {
-                    // Permiso denegado
-                    return false;
-                }
-            }
-            // Permiso otorgado
-            return true;
-        }
-
-
         private async void btnGabarVideo_Clicked(object sender, EventArgs e)
-            {
-            //if (await CheckAndRequestPermissionAsync<Permissions.StorageWrite>())
-            //{
-                var videoOptions = new MediaPickerOptions
-                {
-                    Title = "Capturar video"
-
-                };
-
-                var video = await MediaPicker.CaptureVideoAsync(videoOptions);
-
-                if (video != null)
-                {
-                    using (var stream = await video.OpenReadAsync())
-                    {
-                        videoPath = GetVideoPath(stream);
-
-                        viewVideo.Source = MediaSource.FromUri(new Uri(videoPath));
-                    }
-                }
-            //}
-            //else
-            //{
-            //    await DisplayAlert("Alerta", "No tiene los permisos","ok");
-            //}
-
+        {
+            await TakePhotoAsync();
         }
 
         private string GetVideoPath(Stream stream)
@@ -182,6 +148,12 @@ namespace PM2Examen2Grupo1
         #else
                     return string.Empty;
         #endif
+        }
+
+        private async void btnObtenerUbicacion_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alerta","Obteniendo Ubicacion","OK");
+            await GetCurrentLocation();
         }
 
         private async void btnGrabarAudio_Clicked(object sender, EventArgs e)
@@ -251,5 +223,11 @@ namespace PM2Examen2Grupo1
                 Console.WriteLine("Deteniendo grabación y guardando el audio...");
             }
         }
+        private void btnSalvarUbicacion_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
